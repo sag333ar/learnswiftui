391ad67c986d05720f58326114b134e0b7b871ba
Sagar <sagar.devices@gmail.com>
2024-11-06 11:21:42 +0530

    navigating to a different screen

--- learnswiftui/Ram/Screens/LandingScreen.swift
+++ learnswiftui/Ram/Screens/LandingScreen.swift
@@ -8,27 +8,46 @@
 import SwiftUI
 
 struct LandingScreen: View {
+    @State var goToLogin = false
+    @State var goToSignup = false
+
     var body: some View {
+        NavigationStack {
+            screen
+        }
+    }
+
+    var screen: some View {
         ZStack {
             VStack {
                 Image("logo")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 300)
+
                 Headline(title: "The Freedom of Protection")
             }
 
             VStack {
                 Spacer()
+
                 PrimaryButton(title: "Create An Account") {
                     debugPrint("Hello")
                 }
                 .padding(.bottom, 10)
-                SecondaryButton(title: "Log into Your Account") {
 
+                SecondaryButton(title: "Log into Your Account") {
+                    goToLogin = true
                 }
             }
             .padding()
+        }
+        .navigationTitle("landing screen")
+        .navigationBarHidden(true)
+        .navigationDestination(
+            isPresented: $goToLogin
+        ) {
+            LoginScreen()
         }
     }
 }
---
+++ learnswiftui/Ram/Screens/LoginScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LoginScreen.swift
+//  learnswiftui
+//
+//  Created by Sagar on 06/11/24.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LoginScreen()
+}
